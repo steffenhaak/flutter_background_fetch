3cde6067fdbc150faf6fd70cedafa30ef7e609fb
Steffen Haak <haak@aucentiq.com>
2020-10-05 15:48:13 +0200

    android adaptions

---
+++ android/src/main/java/com/transistorsoft/tsbackgroundfetch/FetchAlarmReceiver.java
@@ -0,0 +1,36 @@
+package com.transistorsoft.tsbackgroundfetch;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.PowerManager;
+import android.util.Log;
+
+import static android.content.Context.POWER_SERVICE;
+
+/**
+ * Created by chris on 2018-01-11.
+ */
+
+public class FetchAlarmReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        PowerManager powerManager = (PowerManager) context.getSystemService(POWER_SERVICE);
+        final PowerManager.WakeLock wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, BackgroundFetch.TAG + "::" + intent.getAction());
+        wakeLock.acquire(60000);
+
+        FetchJobService.CompletionHandler completionHandler = new FetchJobService.CompletionHandler() {
+            @Override
+            public void finish() {
+                if (wakeLock.isHeld()) {
+                    wakeLock.release();
+                    Log.d(BackgroundFetch.TAG, "- FetchAlarmReceiver finish");
+                }
+            }
+        };
+
+        BGTask task = new BGTask(intent.getAction(), completionHandler, 0);
+        BackgroundFetch.getInstance(context.getApplicationContext()).onFetch(task);
+    }
+}
--- updateBaseProject.sh
+++ updateBaseProject.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cp ../transistor-background-fetch/ios/TSBackgroundFetch/TSBackgroundFetch/*.m ./ios/Classes 
 cp ../transistor-background-fetch/ios/TSBackgroundFetch/TSBackgroundFetch/*.h ./ios/Classes
+cp -r ../transistor-background-fetch/android/tsbackgroundfetch/src/main/java/com/transistorsoft/tsbackgroundfetch/ ./android/src/main/java/com/transistorsoft/tsbackgroundfetch
